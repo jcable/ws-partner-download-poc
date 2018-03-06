478193e62a9fed8e3e0860296f4b02946f837143
Julian Cable <cablej01@b1-l0s102204.national.core.bbc.co.uk>
2018-03-06 13:31:03 +0100

    handle aac and mp3 separately

--- lambda/pulse-client/index.js
+++ lambda/pulse-client/index.js
@@ -44,6 +44,7 @@ function nitrorequest(params) {
             accept: 'application/json'
         }
     };
+    console.log("nitro request "+options.path);
     return new Promise(function(resolve,reject){
         http.get(options, function(res) {
             var data = '';
@@ -51,7 +52,15 @@ function nitrorequest(params) {
               data += chunk;
             });
             res.on('end', () => {
-                resolve(JSON.parse(data).nitro.results.items[0]);
+                var results = JSON.parse(data).nitro.results;
+                if(results.total == 1) {
+                    resolve(results.items[0]);
+                }
+                else {
+                    console.log("not available");
+                    console.log(data);
+                    resolve(null);
+                }
             });
         }).on('error', function(e) {
             console.log(e);
@@ -109,7 +118,7 @@ function processEpisode(episodes, bv_promises, config, episode)
         episodes.push(episode);
     }
     else {
-        console.log("unwanted");
+        console.log("unwanted episode");
     }
 }
 
@@ -117,11 +126,48 @@ function processEpisode(episodes, bv_promises, config, episode)
  * we have the clip data - assume this is for publishing a single subject BBC Minute.
  * The idea is to send a message to Caravan which will then fetch the essence and publish it to CDS and email
 */
-function processClip(config, clip)
+function processWantedClip(clip)
 {
+    processWantedClipToValidQueue(clip, process.env.QUEUE);
+    processWantedClipToValidQueue(clip, process.env.TEST_QUEUE);
+    var v = clip.available_versions.version[0];
+    var media_set = v.availabilities.availability[0].media_sets.media_set;
+    var have_mp3 = false;
+    var have_aac = false;
+    for(var i=0; i<media_set.length; i++) {
+        if(media_set[i].name == 'ws-clip-syndication-audio') {
+            have_mp3 = true;
+        }
+        if(media_set[i].name == 'mobile-tablet-main') {
+            have_aac = true;
+        }
+    }
+    if(have_mp3) {
+        processWantedClipToValidQueue(clip, process.env.LIVE_MP3_QUEUE);
+        processWantedClipToValidQueue(clip, process.env.TEST_MP3_QUEUE);
+    }
+    if(have_aac) {
+        processWantedClipToValidQueue(clip, process.env.LIVE_AAC_QUEUE);
+        processWantedClipToValidQueue(clip, process.env.TEST_AAC_QUEUE);
+    }
+}
+
+function processWantedClipToValidQueue(clip, queue)
+{
+    if(queue.includes("aws")) {
+        processWantedClipToQueue(clip, queue);
+    }
+    else {
+        console.log("ignoring queue "+queue);
+    }
+}
+
+function processWantedClipToQueue(clip, queue)
+{
+    console.log("sending to "+queue);
     var params = {
       MessageBody: JSON.stringify(clip),
-      QueueUrl: process.env.QUEUE
+      QueueUrl: queue
     };
     sqs.sendMessage(params, function(err,data){
       if(err) {
@@ -130,6 +176,137 @@ function processClip(config, clip)
         console.log('data:',data.MessageId);
       }
     });
+}
+
+function fixClip(item) {
+    item["clip_of"] = { "pid" : "w13xttlc" };
+    item["genre_groupings"] = 
+    {
+        "genre_group":[
+            {"id":"C00079","type":"iplayer_composite",
+            "genres":{"genre":[{"id":"100007","type":"iplayer_toplevel","$":"News"}]}
+            }
+            ]
+    };
+}
+
+function processIdentifiedClip(item) {
+    if(item.available_versions.available > 0) {
+        var v = item.available_versions.version[0];
+        var status = v.availabilities.availability[0].status;
+        console.log("clip "+item.pid+" version "+v.pid+" availability: "+status);
+        if(status == "available") {
+            console.log("wanted clip");
+            processWantedClip(item);
+        }
+        else {
+            console.log("unwanted clip");
+        }
+    }
+}
+
+function processAudioClip(item) {
+    console.log("clip");
+    if(item.hasOwnProperty("clip_of")) {
+        console.log("clip of "+item.clip_of.pid);
+        if(process.env.CLIP_BRANDS.includes(item.clip_of.pid)) {
+            processIdentifiedClip(item);
+        }        
+        else {
+            console.log("unwanted clip of "+item.clip_of.pid);
+        }
+    }
+    else if(process.env.TITLE.toUpperCase().includes(item.title.trim().toUpperCase())) {
+        console.log("clip title match");
+        fixClip(item);
+        processIdentifiedClip(item);
+    }
+    else {
+        console.log("unwanted clip");
+    }
+}
+
+function processWantedItem(item)
+{
+    processWantedItemToValidQueue(item, process.env.QUEUE);
+    processWantedItemToValidQueue(item, process.env.TEST_QUEUE);
+    var v = item.available_versions.version[0];
+    var media_set = v.availabilities.availability[0].media_sets.media_set;
+    var have_mp3 = false;
+    var have_aac = false;
+    for(var i=0; i<media_set.length; i++) {
+        if(media_set[i].name == 'ws-clip-syndication-audio') {
+            have_mp3 = true;
+        }
+        if(media_set[i].name == 'mobile-tablet-main') {
+            have_aac = true;
+        }
+    }
+    if(have_mp3) {
+        processWantedItemToValidQueue(item, process.env.LIVE_MP3_QUEUE);
+        processWantedItemToValidQueue(item, process.env.TEST_MP3_QUEUE);
+    }
+    if(have_aac) {
+        processWantedItemToValidQueue(item, process.env.LIVE_AAC_QUEUE);
+        processWantedItemToValidQueue(item, process.env.TEST_AAC_QUEUE);
+    }
+}
+
+function processWantedItemToValidQueue(clip, queue)
+{
+    if(queue.includes("aws")) {
+        processWantedItemToQueue(clip, queue);
+    }
+    else {
+        console.log("ignoring queue "+queue);
+    }
+}
+
+function processWantedItemToQueue(item, queue)
+{
+    console.log("sending to "+queue);
+    var params = {
+      MessageBody: JSON.stringify(item),
+      QueueUrl: queue
+    };
+    sqs.sendMessage(params, function(err,data){
+      if(err) {
+        console.log('error:',"Fail Send Message" + err);
+      }else{
+        console.log('data:',data.MessageId);
+      }
+    });
+}
+
+function processIdentifiedEpisode(item) {
+    if(item.available_versions.available > 0) {
+        var v = item.available_versions.version[0];
+        var status = v.availabilities.availability[0].status;
+        console.log("episode "+item.pid+" version "+v.pid+" availability: "+status);
+        if(status == "available") {
+            console.log("wanted episode");
+            processWantedItem(item);
+        }
+        else {
+            console.log("unwanted episode");
+        }
+    }
+}
+
+function processAudioEpisode(item) {
+    console.log("episode");
+    if(item.hasOwnProperty("episode_of")) {
+        console.log("episode of "+item.episode_of.pid);
+        if(process.env.EPISODE_BRANDS.includes(item.episode_of.pid)) {
+            processIdentifiedEpisode(item);
+        }        
+        else {
+            console.log("unwanted episode "+item.clip_of.pid);
+        }
+    }
+    else {
+        console.log("unwanted episode");
+    }
 }
 
 exports.handler = (event, context, callback) => {
@@ -168,13 +345,13 @@ exports.handler = (event, context, callback) => {
         console.log("got "+values.length+" messages");
         for(var i=0; i<values.length; i++) {
             var item = values[i];
-            var item_type = item.item_type;
-            if(item_type == "episode") {
-                processEpisode(episodes, bv_promises, config, values[i]);
-            }
-            else if(item_type == "clip") {
-                if(item.clip_of.pid == process.env.BRAND) {
-                   processClip(config, item);
+            if(item) {
+                var item_type = item.item_type;
+                if(item_type == "episode") {
+                    processAudioEpisode(item);
+                }
+                else if((item_type == "clip") && (item.media_type == "Audio")) {
+                    processAudioClip(item);
                 }
             }
         }
